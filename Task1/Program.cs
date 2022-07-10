9650bb7f988926943ff62189fa20436d47be6ba7
Choppa <denis.afinogenow@yandex.ru>
2022-07-10 12:55:10 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] Numbers(int size)
+{
+    int faggot = 0;
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        faggot = Convert.ToInt32(Console.ReadLine());
+        array[i] = faggot;
+    }
+    return array;
+}
+
+int PositiveNumbers(int[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) j++;
+    }
+    return j;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вводите числа в количестве размера массива");
+
+int[] myarray = Numbers(size);
+
+PositiveNumbers(myarray);
+ShowArray(myarray);
+Console.WriteLine("Количество положительных чисел равно " + PositiveNumbers(myarray));
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+string Function(double a1, double b1, double a2, double b2) // Функция нахождения координат функциий.
+{
+    double x = (b1 + (-b2)) / -(a1 + (-a2));
+    double y = a1 * x + b1;
+
+    string f = Convert.ToString(x);                         // Перевод координат в string чтобы вывести из метода обе 
+    string g = Convert.ToString(y);                         // координаты в одной переменной.
+    string j = f + ";" + g;
+    return j;
+}
+
+void ShowCoor(string j)
+{
+    Console.WriteLine("Координаты точка равны " + j);
+}
+
+Console.WriteLine("Введите числа для первого уравнения");
+int a1 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите числа для второго уравнения");
+int a2 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+ShowCoor(Function(a1, b1, a2, b2));
